4766710feec22c36607f861a887c1fc67509fa18
Romuald Brillout <git@brillout.com>
2022-05-20 23:04:40 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
+import type { UserConfig } from 'vite'
 
 export default {
   plugins: [react(), ssr()],
-}
+  build: { outDir: `${__dirname}/dist` }
+} as UserConfig
